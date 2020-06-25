4e50425aed5641e60a6ba9f56c608918cb2d7548
Githubtesi <tesiyosi7211@gmail.com>
2020-06-26 00:59:26 +0900

    add prime_v1 fix comment Prime num

--- main.py
+++ main.py
@@ -7,9 +7,6 @@
 # Non Prime num
 # [1, 4, 6, 8, 9, 12, 14, 16, 18, 20]
 
-import math
-
-
 def is_prime_v1(num) -> bool:
     if num <= 1:
         return False
@@ -21,66 +18,5 @@ def is_prime_v1(num) -> bool:
     return True
 
 
-def is_prime_v2(num: int) -> bool:
-    """
-    36 = 1* 36
-    36 = 2* 18
-    36 = 3* 12
-    36 = 4* 9
-    36 = 6* 6 <- √n
-    """
-    if num <= 1:
-        return False
-
-    for i in range(2, math.floor(math.sqrt(num) + 1)):
-        if num % i == 0:
-            return False
-
-    return True
-
-
-def is_prime_v2_1(num: int) -> bool:
-    i = 2
-    while i * i <= num:
-        if num % i == 0:
-            return False
-    return True
-
-
-def is_prime_v3(num: int) -> bool:
-    if num <= 1:
-        return False
-
-    if num == 2:
-        return True
-
-    if num % 2 == 0:
-        return False
-
-    for i in range(3, math.floor(math.sqrt(num) + 1), 2):
-        if num % i == 0:
-            return False
-
-    return True
-
-
 if __name__ == '__main__':
-    import random
-    import time
-
-    numbers = [random.randint(0, 1000) for _ in range(100000)]
-    start = time.time()
-
-    for num in numbers:
-        is_prime_v1(num)
-    print('v1', time.time() - start)
-
-    start = time.time()
-    for num in numbers:
-        is_prime_v2(num)
-    print('v2', time.time() - start)
-
-    start = time.time()
-    for num in numbers:
-        is_prime_v3(num)
-    print('v3', time.time() - start)
+    print(is_prime_v1(5))
